4218475e2c0c401dcb4b5755c2c70a32e4806333
BB-69 <129858054+BB-69@users.noreply.github.com>
2025-09-05 12:23:32 +0700

    Initial commit

---
+++ src/bin/fileBuild.rs
@@ -0,0 +1,18 @@
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let path = "data.json";
+    let path_obj = Path::new(path);
+
+    if path_obj.exists() {
+        println!("{} already exists!", path);
+    } else {
+        if let Some(parent) = path_obj.parent() {
+            fs::create_dir_all(parent).expect("Failed to create parent directories");
+        }
+
+        fs::write(path, "{}").expect("Failed to create file");
+        println!("{} created!", path);
+    }
+}
